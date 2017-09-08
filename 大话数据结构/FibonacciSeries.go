ff877e4c2fdd0049d317e31de5090f6a998e673c
吴名 <wuming497735138@gmail.com>
2017-09-09 01:21:25 +0800

    迭代和递归方式实现斐波那契额数列

--- 大话数据结构/FibonacciSeries.go
+++ 大话数据结构/FibonacciSeries.go
@@ -5,3 +5,35 @@ package 大话数据结构
  *2017/8/31 下午8:34
  *斐波那契额数列实现
  */
+
+//版本1:迭代版本
+func Fbi1(i int) int {
+
+	switch i {
+	case 0:
+		return 0
+	case 1:
+		return 1
+	default:
+		arr := make([]int, i+1, i+1)
+		arr[0] = 0
+		arr[1] = 1
+		for j := 2; j <= i; j++ {
+			arr[j] = arr[j-1] + arr[j-2]
+		}
+		return arr[i]
+	}
+}
+
+//版本2:递归版本
+func Fbi2(i int) int {
+	switch i {
+	case 0:
+		return 0
+	case 1:
+		return 1
+	default:
+		//递归调用
+		return Fbi2(i-1) + Fbi2(i-2)
+	}
+}
---
+++ 大话数据结构/test_test.go
@@ -0,0 +1,17 @@
+package 大话数据结构
+
+import "testing"
+
+/**
+ * WuMing 
+ *2017/9/9 上午12:08
+ *
+ */
+
+func Test_Fbi1(t *testing.T) {
+	t.Logf("结果:%v",Fbi1(40))
+}
+
+func Test_Fbi2(t *testing.T) {
+	t.Logf("结果:%v",Fbi2(40))
+}
