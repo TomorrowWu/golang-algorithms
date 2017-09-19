2b517e03f9af6226414955e246c33606bf24c02b
吴名 <wuming497735138@gmail.com>
2017-09-19 21:24:00 +0800

    优化,减少循环次数

--- 3色球问题/colorMatch.go
+++ 3色球问题/colorMatch.go
@@ -17,17 +17,42 @@ import (
 三色球问题。有红、黄、绿三种颜色的球，其中红球3个，黄球3个，绿球6个。现将这12个球混放在一个盒子里，从中任意摸出8个球，编程计算摸出球的各种颜色搭配。
 */
 func computeMatch(redCount, yellowCount, greenCount, total int) int {
-	if total <= 0 || total > redCount+yellowCount+greenCount {
+	if redCount < 0 || yellowCount < 0 || greenCount < 0 || total <= 0 || total > redCount+yellowCount+greenCount {
 		return 0
 	}
+
 	num := 0
 	for r := 0; r <= redCount; r++ {
-		for y := 0; y <= yellowCount; y++ {
-			for g := 0; g <= greenCount; g++ {
-				if r+y+g == total {
-					log.Printf("red:%v yellow:%v green:%v", r, y, g)
+
+		if yellowCount+greenCount < total-r {
+			continue
+		}
+
+		if r == total {
+			num++
+			log.Printf("red:%v yellow:%v green:%v", r, 0, 0)
+			break
+		} else if r < total {
+			for y := 0; y <= yellowCount; y++ {
+
+				if r+y == total {
 					num++
+					log.Printf("red:%v yellow:%v green:%v", r, y, 0)
+					break
+				} else if r+y < total {
+					needGreen := total - r - y
+					if needGreen > 0 && greenCount >= needGreen {
+						log.Printf("red:%v yellow:%v green:%v", r, y, total-r-y)
+						num++
+					}
 				}
+
+				//for g := 0; g <= greenCount; g++ {
+				//	if r+y+g == total {
+				//		log.Printf("red:%v yellow:%v green:%v", r, y, g)
+				//		num++
+				//	}
+				//}
 			}
 		}
 	}
--- 3色球问题/test_test.go
+++ 3色球问题/test_test.go
@@ -11,6 +11,9 @@ import (
  */
 
 func Test_computeMatch(t *testing.T) {
-	n := computeMatch(1,1,1,2)
+	//n := computeMatch(1,1,1,2)
+	//n := computeMatch(3,3,6,8)
+	//n := computeMatch(3,3,6,2)
+	n := computeMatch(3000,3000,6001,12000)
 	t.Logf("排列组合:%v",n)
 }
