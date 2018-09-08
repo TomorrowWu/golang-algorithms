4abd7dbc695c0cfec601e432260b07d0d44da070
吴名 <wuming497735138@gmail.com>
2018-09-08 17:04:07 +0800

    leetcode 0887  鸡蛋落

--- leetcode/0887.super-egg-drop/main_test.go
+++ leetcode/0887.super-egg-drop/main_test.go
@@ -1,1 +1,10 @@
 package main
+
+import "testing"
+
+func Test_superEggDrop(t *testing.T) {
+	t.Logf("最小移动次数:%d\n", superEggDrop(1, 2))  //2
+	t.Logf("最小移动次数:%d\n", superEggDrop(2, 6))  //3
+	t.Logf("最小移动次数:%d\n", superEggDrop(3, 14)) //4
+	t.Logf("最小移动次数:%d\n", superEggDrop(1, 3))  //3
+}
