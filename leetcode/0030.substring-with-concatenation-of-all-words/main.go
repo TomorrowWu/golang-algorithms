4095838008a4efbb5b3d89b01468f7dd712a02b3
吴名 <wuming497735138@gmail.com>
2018-09-13 15:17:08 +0800

    leetcode 0020. 与所有单词相关联的字串,自研的算法,2428ms/174测试用例

--- leetcode/0030.substring-with-concatenation-of-all-words/main.go
+++ leetcode/0030.substring-with-concatenation-of-all-words/main.go
@@ -1,1 +1,57 @@
 package main
+
+import "strings"
+
+//本人自己写的算法(执行用时：2428 ms)
+func findSubstring(s string, words []string) []int {
+	result := make([]int, 0)
+
+	wordsLen := 0
+	heads := make(map[uint8]bool, len(words))
+	tails := make(map[uint8]bool, len(words))
+	for _, word := range words {
+		if len(word) > 0 {
+			wordsLen += len(word)
+			heads[word[0]] = true
+			tails[word[len(word)-1]] = true
+		}
+	}
+
+	strLen := len(s)
+	if strLen < wordsLen || wordsLen == 0 {
+		return result
+	}
+
+	for i, a := range s {
+		//剩下的子串长度小于words长度
+		if strLen-i < wordsLen {
+			break
+		}
+
+		//字符和head相等
+		isHead, _ := heads[uint8(a)]
+		isTail, _ := tails[s[i+wordsLen-1]]
+		//头和尾符合words要求
+		if isHead && isTail {
+			newStr := s[i : i+wordsLen]
+			//比较子串是否等于words的组合
+			newWords := append([]string{}, words[0:]...)
+			for j := 0; j < len(words); j++ {
+				for k, word := range newWords {
+					//每次去掉一个word,words中每一个都得有
+					if strings.HasPrefix(newStr, word) {
+						newStr = strings.TrimPrefix(newStr, word)
+						newWords = append(newWords[:k], newWords[k+1:]...)
+						break
+					}
+				}
+			}
+
+			if len(newStr) == 0 {
+				result = append(result, i)
+			}
+		}
+	}
+
+	return result
+}
--- leetcode/0030.substring-with-concatenation-of-all-words/main_test.go
+++ leetcode/0030.substring-with-concatenation-of-all-words/main_test.go
@@ -1,1 +1,11 @@
 package main
+
+import "testing"
+
+func Test_findSubstring(t *testing.T) {
+	t.Logf("result=%v\n", findSubstring("barfoothefoobarman", []string{"foo", "bar"}))
+	t.Logf("result=%v\n", findSubstring("wordgoodstudentgoodword", []string{"word", "student"}))
+	t.Logf("result=%v\n", findSubstring("a", []string{}))
+	t.Logf("result=%v\n", findSubstring("wordgoodgoodgoodbestword", []string{"word", "good", "best", "word"}))
+	t.Logf("result=%v\n", findSubstring("wordgoodgoodgoodbestword", []string{"word", "good", "best", "good"}))
+}
