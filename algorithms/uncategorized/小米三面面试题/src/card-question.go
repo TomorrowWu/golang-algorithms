2e0394b4a743c11c339158e714291f99fa7446fb
吴名 <wuming497735138@gmail.com>
2018-10-12 20:19:36 +0800

    an algorithm question of Xiaomi

---
+++ algorithms/uncategorized/小米三面面试题/src/card-question.go
@@ -0,0 +1,22 @@
+package main
+
+func positiveSolution(n int) []int {
+	result := make([]int, n)
+	//next表示是否需要跳过这位数字
+	next := false
+	//i就是待标记的数字
+	i := 1
+	for i <= n {
+		for p := 0; p < n; p++ {
+			if result[p] == 0 {
+				if !next {
+					result[p] = i
+					i++
+				}
+				next = !next
+			}
+		}
+	}
+
+	return result
+}
---
+++ algorithms/uncategorized/小米三面面试题/src/card_test.go
@@ -0,0 +1,21 @@
+package main
+
+import "testing"
+
+func TestPositiveSolution(t *testing.T) {
+
+	testCase := []struct {
+		n         int
+		expectArr []int
+	}{
+		{
+			n:         5,
+			expectArr: []int{1, 5, 2, 4, 3},
+		},
+	}
+
+	for _, c := range testCase {
+		result := positiveSolution(c.n)
+		t.Logf("expect result:%v, actual result:%v", c.expectArr, result)
+	}
+}
