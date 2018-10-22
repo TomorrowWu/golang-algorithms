f40d7a3e5817758a0395a9dcadf59882cbaf8993
吴名 <wuming497735138@gmail.com>
2018-10-22 19:13:38 +0800

    LeetCode 232 implement-queue-using-stacks

--- leetcode/0232.implement-queue-using-stacks/src/queue-by-stacks.go
+++ leetcode/0232.implement-queue-using-stacks/src/queue-by-stacks.go
@@ -9,34 +9,80 @@ package src
  * param_4 := obj.Empty();
  */
 
+// MyQueue defines a queue by two stacks
 type MyQueue struct {
+	a, b *stack
 }
 
 /** Initialize your data structure here. */
 func Constructor() MyQueue {
-
-	return MyQueue{}
+	return MyQueue{
+		a: newStack(),
+		b: newStack(),
+	}
 }
 
-/** Push element x to the back of queue. */
-func (this *MyQueue) Push(x int) {
-
+// Push element x to the back of queue.
+func (queue *MyQueue) Push(x int) {
+	queue.a.push(x)
 }
 
-/** Removes the element from in front of queue and returns that element. */
-func (this *MyQueue) Pop() int {
-
-	return -1
+// Pop Removes the element from in front of queue and returns that element.
+func (queue *MyQueue) Pop() int {
+	if queue.b.isEmpty() {
+		//优化: 栈a中留一个元素供pop,可以少一次操作
+		for queue.a.len() > 1 {
+			queue.b.push(queue.a.pop())
+		}
+		return queue.a.pop()
+	}
+	return queue.b.pop()
 }
 
-/** Get the front element. */
-func (this *MyQueue) Peek() int {
-
-	return -1
+// Peek Get the front element.
+func (queue *MyQueue) Peek() int {
+	if queue.b.isEmpty() {
+		if queue.a.isEmpty() {
+			return -1
+		} else {
+			return queue.a.nums[0]
+		}
+	}
+	return queue.b.nums[queue.b.len()-1]
 }
 
-/** Returns whether the queue is empty. */
-func (this *MyQueue) Empty() bool {
+// Empty Returns whether the queue is empty.
+func (queue *MyQueue) Empty() bool {
+	return queue.a.isEmpty() && queue.b.isEmpty()
+}
 
-	return false
+// stack defines a stack
+type stack struct {
+	nums []int
+}
+
+func newStack() *stack {
+	return &stack{
+		nums: []int{},
+	}
+}
+
+func (s *stack) push(n int) {
+	s.nums = append(s.nums, n)
+}
+
+func (s *stack) pop() int {
+	if s.isEmpty() {
+		return -1
+	}
+	res := s.nums[len(s.nums)-1]
+	s.nums = s.nums[:len(s.nums)-1]
+	return res
+}
+
+func (s *stack) len() int {
+	return len(s.nums)
+}
+func (s *stack) isEmpty() bool {
+	return s.len() == 0
 }
---
+++ leetcode/0232.implement-queue-using-stacks/src/queue-by-stacks_test.go
@@ -0,0 +1,319 @@
+package src
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestConstructor(t *testing.T) {
+	tests := []struct {
+		name string
+		want MyQueue
+	}{
+		{
+			name: "queue-constructor-1",
+			want: MyQueue{
+				a: &stack{
+					nums: []int{},
+				},
+				b: &stack{
+					nums: []int{},
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Constructor(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Constructor() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestMyQueue_Push(t *testing.T) {
+	type fields struct {
+		a *stack
+		b *stack
+	}
+	type args struct {
+		x int
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+	}{
+		{
+			name: "queue-push-1",
+			fields: fields{
+				a: &stack{
+					nums: []int{},
+				},
+				b: &stack{
+					nums: []int{},
+				},
+			},
+			args: args{
+				x: 5,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			queue := &MyQueue{
+				a: tt.fields.a,
+				b: tt.fields.b,
+			}
+			queue.Push(tt.args.x)
+		})
+	}
+}
+
+func TestMyQueue_Pop(t *testing.T) {
+	type fields struct {
+		a *stack
+		b *stack
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   int
+	}{
+		{
+			name: "queue-pop-1",
+			fields: fields{
+				a: &stack{
+					nums: []int{3},
+				},
+				b: &stack{
+					nums: []int{2, 1},
+				},
+			},
+			want: 1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			queue := &MyQueue{
+				a: tt.fields.a,
+				b: tt.fields.b,
+			}
+			if got := queue.Pop(); got != tt.want {
+				t.Errorf("MyQueue.Pop() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestMyQueue_Peek(t *testing.T) {
+	type fields struct {
+		a *stack
+		b *stack
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   int
+	}{
+		{
+			name: "test-peek-1",
+			fields: fields{
+				a: &stack{
+					nums: []int{3},
+				},
+				b: &stack{
+					nums: []int{2, 1},
+				},
+			},
+			want: 1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			queue := &MyQueue{
+				a: tt.fields.a,
+				b: tt.fields.b,
+			}
+			if got := queue.Peek(); got != tt.want {
+				t.Errorf("MyQueue.Peek() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestMyQueue_Empty(t *testing.T) {
+	type fields struct {
+		a *stack
+		b *stack
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   bool
+	}{
+		{
+			name: "queue-Empty-1",
+			fields: fields{
+				a: &stack{
+					nums: []int{1},
+				},
+				b: &stack{
+					nums: []int{},
+				},
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			queue := &MyQueue{
+				a: tt.fields.a,
+				b: tt.fields.b,
+			}
+			if got := queue.Empty(); got != tt.want {
+				t.Errorf("MyQueue.Empty() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_newStack(t *testing.T) {
+	tests := []struct {
+		name string
+		want *stack
+	}{
+		{
+			name: "stack-1",
+			want: &stack{
+				nums: []int{},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := newStack(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("newStack() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_stack_push(t *testing.T) {
+	type fields struct {
+		nums []int
+	}
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+	}{
+		{
+			name: "stack-push-1",
+			fields: fields{
+				nums: []int{},
+			},
+			args: args{
+				n: 0,
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := &stack{
+				nums: tt.fields.nums,
+			}
+			s.push(tt.args.n)
+		})
+	}
+}
+
+func Test_stack_pop(t *testing.T) {
+	type fields struct {
+		nums []int
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   int
+	}{
+		{
+			name: "stack-pop-1",
+			fields: fields{
+				nums: []int{5},
+			},
+			want: 5,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := &stack{
+				nums: tt.fields.nums,
+			}
+			if got := s.pop(); got != tt.want {
+				t.Errorf("stack.pop() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_stack_len(t *testing.T) {
+	type fields struct {
+		nums []int
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   int
+	}{
+		{
+			name: "stack-len-1",
+			fields: fields{
+				nums: []int{5},
+			},
+			want: 1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := &stack{
+				nums: tt.fields.nums,
+			}
+			if got := s.len(); got != tt.want {
+				t.Errorf("stack.len() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_stack_isEmpty(t *testing.T) {
+	type fields struct {
+		nums []int
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   bool
+	}{
+		{
+			name: "stack-isEmpty-1",
+			fields: fields{
+				nums: []int{5},
+			},
+			want: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := &stack{
+				nums: tt.fields.nums,
+			}
+			if got := s.isEmpty(); got != tt.want {
+				t.Errorf("stack.isEmpty() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
