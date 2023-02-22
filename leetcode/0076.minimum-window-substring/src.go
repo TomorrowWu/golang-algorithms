7208d1c16daab2e6547f1dbcb142b8a14a92820d
ming.wu <ming.wu@ushow.media>
2023-02-22 14:08:02 +0800

    0069最小覆盖子串

--- leetcode/0076.minimum-window-substring/src.go
+++ leetcode/0076.minimum-window-substring/src.go
@@ -1,1 +1,38 @@
 package _076_minimum_window_substring
+
+func minWindow(s string, t string) string {
+	cnt := map[byte]int{}
+	for i := range t {
+		cnt[t[i]]++
+	}
+
+	cntAns := map[byte]int{}
+	l, r := -1, -1
+	left := 0
+	for right := range s {
+		cntAns[s[right]]++
+		for mapContain(cntAns, cnt) {
+			if l == -1 || right-left < r-l {
+				l, r = left, right
+			}
+			cntAns[s[left]]--
+			left++
+		}
+	}
+	if l == -1 {
+		return ""
+	}
+	return s[l : r+1]
+}
+
+func mapContain(a, b map[byte]int) bool {
+	if len(b) > len(a) {
+		return false
+	}
+	for k, v := range b {
+		if a[k] < v {
+			return false
+		}
+	}
+	return true
+}
