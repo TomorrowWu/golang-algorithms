6d058e4639dc6d5d649d6294007029b10bd05e36
ming.wu <ming.wu@ushow.media>
2023-02-24 21:48:49 +0800

    438. 找到字符串中所有字母异位词

--- leetcode/0438.find-all-anagrams-in-a-string/src.go
+++ leetcode/0438.find-all-anagrams-in-a-string/src.go
@@ -1,1 +1,28 @@
 package _438_find_all_anagrams_in_a_string
+
+func findAnagrams(s string, p string) []int {
+	l1, l2 := len(s), len(p)
+	if l1 < l2 {
+		return []int{}
+	}
+
+	ans := []int{}
+	c1 := [26]int{}
+	c2 := [26]int{}
+	for i, ch := range p {
+		c1[ch-'a']++
+		c2[s[i]-'a']++
+	}
+	if c1 == c2 {
+		ans = append(ans, 0)
+	}
+
+	for i := 1; i <= l1-l2; i++ {
+		c2[s[i-1]-'a']--
+		c2[s[i+l2-1]-'a']++
+		if c2 == c1 {
+			ans = append(ans, i)
+		}
+	}
+	return ans
+}
--- leetcode/0438.find-all-anagrams-in-a-string/src_test.go
+++ leetcode/0438.find-all-anagrams-in-a-string/src_test.go
@@ -1,1 +1,34 @@
 package _438_find_all_anagrams_in_a_string
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_findAnagrams(t *testing.T) {
+	type args struct {
+		s string
+		p string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			name: "1",
+			args: args{
+				s: "cbaebabacd",
+				p: "abc",
+			},
+			want: []int{0, 6},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findAnagrams(tt.args.s, tt.args.p); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("findAnagrams() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
